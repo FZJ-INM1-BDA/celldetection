e9a1fbda770776f0dc1d1dc20be21801ba3e2ddc
ericup <eric@upschulte.com>
2021-07-08 17:56:36 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
+""" CellDetection is a package for efficient and robust cell detection with PyTorch.
+
+CellDetection is licensed under Apache License, Version 2.0.
+
+"""
+
+DOCSTRING = (__doc__ or '').split("\n")
+
 from setuptools import setup
 
 requirements = [
@@ -5,8 +13,8 @@ requirements = [
     'scikit-image',
     'scipy',
     'opencv-python',
-    'torch',
-    'torchvision',
+    'torch>=1.9.0',
+    'torchvision>=0.10.0',
     'seaborn',
     'matplotlib',
     'tqdm',
@@ -18,8 +26,9 @@ setup(
     author="Eric Upschulte",
     author_email='e.upschulte@fz-juelich.de',
     name='celldetection',
-    version='0.1.0',
-    description='Cell Detection with PyTorch',
+    version='0.2.0',
+    description=DOCSTRING[0],
+    long_description="\n".join(DOCSTRING[2:]),
     url='https://celldetection.org',
     packages=['celldetection', 'celldetection.data', 'celldetection.models', 'celldetection.ops', 'celldetection.util',
               'celldetection.visualization'],
