94b6163263868079f1473472a2082b71e476d289
ericup <eric@upschulte.com>
2022-02-15 10:05:10 +0100

    Add replay cache

--- celldetection/models/commons.py
+++ celldetection/models/commons.py
@@ -1,8 +1,10 @@
+import numpy as np
+import torch
 import torch.nn as nn
 from torch import Tensor, tanh, no_grad, as_tensor
-from ..util.util import get_device, num_params, gaussian_kernel
+from ..util.util import gaussian_kernel
 
-__all__ = ['TwoConvBnRelu', 'ScaledTanh', 'GaussianBlur']
+__all__ = ['TwoConvBnRelu', 'ScaledTanh', 'GaussianBlur', 'ReplayCache']
 
 
 class GaussianBlur(nn.Conv2d):
@@ -38,3 +40,29 @@ class ScaledTanh(nn.Module):
 
     def forward(self, inputs: Tensor) -> Tensor:
         return tanh(inputs) * self.factor
+
+
+class ReplayCache:
+    def __init__(self, size=128):
+        self.cache = []
+        self.size = size
+
+    def __len__(self):
+        return len(self.cache)
+
+    def is_empty(self):
+        return len(self) <= 0
+
+    def append(self, x, fraction=.5):
+        lx = len(x)
+        for i in np.random.choice(np.arange(lx), int(lx * fraction), replace=False):
+            self.cache.append(x[i].detach())
+        while len(self) > self.size:
+            del self.cache[np.random.randint(0, len(self))]
+
+    def __call__(self, num):
+        if self.is_empty():
+            return None
+        return torch.stack([self.cache[i] for i in np.random.randint(0, len(self), num)], 0)
+
+    add = append
