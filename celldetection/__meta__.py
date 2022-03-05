2373b6944c3bf1309e90bf6baa785bf193caa617
ericup <eric@upschulte.com>
2022-03-05 14:02:25 +0100

    Update version

--- celldetection/__meta__.py
+++ celldetection/__meta__.py
@@ -1,5 +1,5 @@
 __title__ = 'CellDetection'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __author__ = 'Eric Upschulte'
 __email__ = 'e.upschulte@fz-juelich.de'
 __license__ = 'Apache License, Version 2.0'
