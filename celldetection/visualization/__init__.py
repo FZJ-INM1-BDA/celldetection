3109c3f6269ead10f68052277075a50b5cfb4465
ericup <eric@upschulte.com>
2022-09-22 11:15:05 +0200

    Add label colormap

--- celldetection/visualization/__init__.py
+++ celldetection/visualization/__init__.py
@@ -1,1 +1,2 @@
 from .images import *
+from .cmaps import *
---
+++ celldetection/visualization/cmaps.py
@@ -0,0 +1,40 @@
+from matplotlib.pyplot import cm
+import numpy as np
+from numpy import ndarray
+from typing import Union
+
+__all__ = ['label_cmap']
+
+
+def label_cmap(labels: ndarray, colors: Union[str, ndarray] = 'tab20c', zero_val: Union[float, tuple, list] = 0.,
+               rgba: bool = True, alpha: float = None):
+    """Label colormap.
+
+    Applies a colormap to a label image.
+
+    Args:
+        labels: Label image. Typically Array[h, w].
+        colors: One of ['Pastel1', 'Pastel2', 'Paired', 'Accent', 'Dark2', 'Set1', 'Set2', 'Set3', 'tab10', 'tab20',
+            'tab20b', 'tab20c'] (see matplotlib's qualitative colormaps) or Array[n, c].
+        zero_val: Special color for the zero label (usually background).
+        rgba: Whether to add an alpha channel to rgb colors.
+        alpha: Specific alpha value.
+
+    Returns:
+        Mapped labels. E.g. rgba mapping Array[h, w] -> Array[h, w, 4].
+    """
+    assert np.integer(labels), 'Pass labels as an integer array.'
+    if isinstance(colors, str):
+        colors = cm.get_cmap(colors).colors
+    if isinstance(colors, (tuple, list)):
+        colors = np.array(colors)
+    if rgba and colors.shape[1] == 3:
+        colors = np.concatenate((colors, np.ones((len(colors), 1))), -1)
+    if alpha is not None and colors.shape[1] == 4:
+        colors[:, -1] = alpha
+    if zero_val is not None:
+        labels = np.copy(labels)
+        m = labels != 0
+        labels[m] = labels[m] % len(colors) + 1
+        colors = np.concatenate((np.ones_like(colors[:1]) * zero_val, colors))
+    return colors[labels]
