ae40e16924def60259aa9b6d969a4843a2707e2b
ericup <eric@upschulte.com>
2023-03-22 17:06:39 +0100

    Update visualization functions

--- celldetection/visualization/images.py
+++ celldetection/visualization/images.py
@@ -1,59 +1,165 @@
 from matplotlib import pyplot as plt
 from matplotlib import patches
 import matplotlib.patheffects as path_effects
+from matplotlib.axes import SubplotBase
 import numpy as np
 import seaborn as sbn
-from torch import Tensor
+from torch import Tensor, squeeze, as_tensor
+from torchvision.utils import make_grid
+from typing import Union, List
 from ..util.util import asnumpy
+from ..data import channels_first2channels_last, channels_last2channels_first
 
-__all__ = ['imshow', 'plot_mask', 'plot_box', 'plot_text', 'quiver_plot', 'show_detection', 'save_fig', 'imshow_grid']
+__all__ = []
 
 
-def imshow(image, figsize=None, **kw):
+def register(obj):
+    __all__.append(obj.__name__)
+    return obj
+
+
+def _fig(figsize):
+    if figsize is not None:
+        plt.figure(None, figsize)
+
+
+def _title(titles, idx=None):
+    if isinstance(titles, str):
+        plt.title(titles)
+    elif idx is not None and isinstance(titles, (list, tuple)):
+        plt.title(titles[idx])
+    else:
+        plt.title(str(titles))
+
+
+def _tight(tight=True):
+    if tight is True:
+        tight = {}
+    if tight:
+        plt.tight_layout(**tight)
+
+
+def _im_args(images):
+    n = len(images)
+    if n == 1 and isinstance(images[0], (list, tuple)):
+        images, = images
+        n = len(images)
+    return n, images
+
+
+def _unpack(image):
+    if isinstance(image, Tensor):
+        if image.ndim == 4:  # n, c, h, w
+            if image.shape[0] == 1:
+                image = squeeze(image, dim=0)
+            else:
+                image = make_grid(image)
+        image = asnumpy(image)
+        if image.ndim == 3:  # c, h, w
+            image = channels_first2channels_last(image)
+    if isinstance(image, np.ndarray):
+        if image.ndim == 4:  # n, h, w, c
+            if image.shape[0] == 1:
+                image = np.squeeze(image, 0)
+            else:
+                image = _unpack(as_tensor(channels_last2channels_first(image, has_batch=True)))
+        if image.ndim == 3 and image.shape[2] == 1:
+            image = np.squeeze(image, 2)
+    return image
+
+
+@register
+def get_axes(fig=None) -> List['SubplotBase']:
+    """Get current pyplot axes.
+
+    Args:
+        fig: Optional Figure.
+
+    Returns:
+        List of Axes.
+    """
+    if fig is None:
+        fig = plt.gcf()
+    return fig.get_axes()
+
+
+@register
+def imshow(image: Union[np.ndarray, Tensor], figsize=None, **kw):
     """Imshow.
 
     PyPlot's imshow function with benefits.
 
     Args:
-        image: Image as Array[h, w], Array[h, w, c], Array[1, h, w] or Array[1, h, w, c]. Images without channels or
-            just one channel are plotted as grayscale images by default.
+        image: Image. Valid Formats:
+            Array[h, w], Array[h, w, c] or Array[n, h, w, c],
+            Tensor[h, w], Tensor[c, h, w] or Tensor[n, c, h, w].
+            Images without channels or just one channel are plotted as grayscale images by default.
         figsize: Figure size. If specified, a new ``plt.figure(figsize=figsize)`` is created.
         **kw: Imshow keyword arguments.
 
     """
-    if figsize is not None:
-        plt.figure(None, figsize)
-    if isinstance(image, Tensor):
-        image = asnumpy(image)
-    if image.ndim == 3 and 1 in image.shape:
-        if image.shape[-1] == 1:
-            image = np.squeeze(image, -1)
-        elif image.shape[0] == 1:
-            image = np.squeeze(image, 0)
+    _fig(figsize)
+    image = _unpack(image)
     if image.ndim == 2 and 'cmap' not in kw.keys():
         kw['cmap'] = 'gray'
     plt.imshow(image, **kw)
     plt.grid(0)
 
 
-def plot_text(class_name, x, y, score=None, color='black', stroke_width=5, stroke_color='w'):
+@register
+def plot_text(text, x, y, color='black', stroke_width=5, stroke_color='w'):
     ax = plt.gca()
-    name = f'{class_name}'
-    if score is not None:
-        name += f': {np.round(score * 100, 1)}%'
-    txt = ax.text(x, y, name, color=color)
+    txt = ax.text(x, y, str(text), color=color)
     if stroke_width is not None and stroke_width > 0:
         txt.set_path_effects([path_effects.withStroke(linewidth=stroke_width, foreground=stroke_color)])
 
 
-def plot_box(x_min, y_min, x_max, y_max, score=None, class_name='score'):
+def _score_text(score, cls, cls_names):
+    if isinstance(score, Tensor):
+        score = asnumpy(score)
+    if isinstance(cls, int) and cls_names is not None:
+        cls = cls_names[cls]
+    txt = ''
+    if cls is not None:
+        txt += f'{cls}: '
+    txt += f'{np.round(score * 100, 1)}%'
+    return txt
+
+
+def _score_texts(scores, classes: Union[List[str], List[int]] = None, class_names: dict = None):
+    texts = []
+    for i in range(len(scores)):
+        cls = None if classes is None else classes[i]
+        texts.append(_score_text(scores[i], cls, class_names))
+    return texts
+
+
+@register
+def plot_score(score: float, x, y, cls: Union[int, str] = None, cls_names: dict = None, **kwargs):
+    plot_text(text=_score_text(score, cls=cls, cls_names=cls_names), x=x, y=y, **kwargs)
+
+
+@register
+def plot_box(x_min, y_min, x_max, y_max, linewidth=1, edgecolor='#4AF626',
+             facecolor='none', text=None, **kwargs):
     ax = plt.gca()
-    ax.add_patch(patches.Rectangle((x_min, y_min), x_max - x_min, y_max - y_min, linewidth=1, edgecolor='#4AF626',
-                                   facecolor='none'))
-    if score is not None:
-        plot_text(class_name, x_min, y_min - 5, score=score)
+    ax.add_patch(
+        patches.Rectangle((x_min, y_min), x_max - x_min, y_max - y_min, linewidth=linewidth, edgecolor=edgecolor,
+                          facecolor=facecolor, **kwargs))
+    if text is not None:
+        plot_text(text, x_min, y_min, **kwargs.get('text_kwargs', {}))
 
 
+@register
+def plot_boxes(boxes, texts: List[str] = None, **kwargs):
+    for box_i, box in enumerate(boxes):
+        if isinstance(box, Tensor):
+            box = asnumpy(box)
+        text = None if texts is None else texts[box_i]
+        plot_box(*box, text=text, **kwargs)
+
+
+@register
 def plot_mask(mask, alpha=1):
     mask_im = ((mask * 255)).astype('uint8')
     zm = np.zeros_like(mask_im)
@@ -62,29 +168,48 @@ def plot_mask(mask, alpha=1):
     plt.grid(0)
 
 
+@register
+def plot_contours(contours, contour_line_width=2, contour_linestyle='-', fill=.2, color=None,
+                  texts: list = None, **kwargs):
+    if isinstance(contours, Tensor):
+        contours = asnumpy(contours)
+    text_kwargs = dict(kwargs.get('text_kwargs', {}))
+    for c_i, c_ in enumerate(contours):
+        kw = dict(kwargs)
+        color_ = color
+        if isinstance(color, list):
+            color_ = color[0]
+        if fill:
+            fill_kw = dict(kwargs.get('fill_kwargs', {}))
+            if color_ is not None:
+                fill_kw['color'] = color_
+            f, = plt.fill(c_[:, 0], c_[:, 1], alpha=fill, **fill_kw)
+            if color_ is None:
+                color_ = f.get_facecolor()[:3]
+        c_ = np.concatenate((c_, c_[:1]), 0)  # close contour
+        plt.plot(c_[:, 0], c_[:, 1], linestyle=contour_linestyle, linewidth=contour_line_width, color=color_, **kw)
+        if texts is not None:
+            c_min = np.argmin(c_[:, 1])
+            plot_text(texts[c_i], c_[c_min, 0], c_[c_min, 1], **text_kwargs)
+
+
+@register
 def show_detection(image=None, contours=None, coordinates=None, boxes=None, scores=None, masks=None,
-                   figsize=None, label_stack=None, class_name='score', contour_line_width=2, contour_linestyle='-',
-                   fill=.2, cmap=...):
-    if figsize is not None:
-        plt.figure(None, figsize)
+                   figsize=None, label_stack=None, classes: Union[List[str], List[int]] = None,
+                   class_names: dict = None, contour_line_width=2, contour_linestyle='-',
+                   fill=.2, cmap=..., **kwargs):
+    _fig(figsize)
     plt.grid(0)
     if image is not None:
         imshow(image, **({} if cmap is ... else dict(cmap=cmap)))
+    if scores is not None:
+        score_texts = _score_texts(scores, classes=classes, class_names=class_names)
     if contours is not None:
-        for c_i, c_ in enumerate(contours):
-            kw = {}
-            if fill:
-                f, = plt.fill(c_[:, 0], c_[:, 1], alpha=fill)
-                kw['color'] = f.get_facecolor()[:3]
-            c_ = np.concatenate((c_, c_[:1]), 0)  # close contour
-            plt.plot(c_[:, 0], c_[:, 1], linestyle=contour_linestyle, linewidth=contour_line_width, **kw)
-            if scores is not None and boxes is None:
-                c_min = np.argmin(c_[:, 1])
-                if isinstance(class_name, str):
-                    cn = class_name
-                else:
-                    cn = class_name[c_i]
-                plot_text(cn, c_[c_min, 0], c_[c_min, 1], scores[c_i])
+        con_kw = kwargs.get('contour_kwargs', {})
+        if scores is not None and boxes is None and 'texts' not in con_kw:
+            con_kw['texts'] = score_texts
+        plot_contours(contours, contour_line_width=contour_line_width, contour_linestyle=contour_linestyle, fill=fill,
+                      **con_kw)
     if masks is not None:
         for mask in masks:
             plot_mask(np.squeeze(mask))
@@ -97,10 +222,13 @@ def show_detection(image=None, contours=None, coordinates=None, boxes=None, scor
     if coordinates is not None:
         plt.scatter(coordinates[:, 0], coordinates[:, 1], marker='+')
     if boxes is not None:
-        for box_i, box in enumerate(boxes):
-            plot_box(*box, score=scores[box_i] if scores is not None else scores, class_name=class_name)
+        box_kw = kwargs.get('box_kwargs', {})
+        if scores is not None and 'texts' not in box_kw:
+            box_kw['texts'] = score_texts
+        plot_boxes(boxes, **box_kw)
 
 
+@register
 def save_fig(filename, close=True):
     """Save Figure.
 
@@ -116,6 +244,7 @@ def save_fig(filename, close=True):
         plt.close('all')
 
 
+@register
 def quiver_plot(vector_field, image=None, cmap='gray', figsize=None, qcmap='twilight', linewidth=.125, width=.19,
                 alpha=.7):
     """Quiver plot.
@@ -148,6 +277,7 @@ def quiver_plot(vector_field, image=None, cmap='gray', figsize=None, qcmap='twil
     plt.grid(0)
 
 
+@register
 def imshow_grid(*images, titles=None, figsize=(3, 3), tight=True, **kwargs):
     """Imshow grid.
 
@@ -161,17 +291,60 @@ def imshow_grid(*images, titles=None, figsize=(3, 3), tight=True, **kwargs):
         **kwargs: Keyword arguments for ``cd.imshow``.
 
     """
-    if len(images) == 1 and isinstance(images[0], (list, tuple)):
-        images, = images
-    n = int(np.ceil(np.sqrt(len(images))))
+    n, images = _im_args(images)
+    n = int(np.ceil(np.sqrt(n)))
     if figsize is not None:
         plt.figure(None, (figsize[0] * n, figsize[1] * n))
     for i, img in enumerate(images):
         plt.subplot(n, n, i + 1)
         imshow(img, **kwargs)
-        if tight:
-            plt.tight_layout()
-        if isinstance(titles, str):
-            plt.title(titles)
-        elif isinstance(titles, (list, tuple)):
-            plt.title(titles[i])
+        _tight(tight)
+        _title(titles, i)
+
+
+@register
+def imshow_row(*images, titles=None, figsize=(3, 3), tight=True, **kwargs):
+    """Imshow row.
+
+    Display a list of images in a row.
+
+    Args:
+        *images: Images.
+        titles: Titles. Either string or list of strings (one for each image).
+        figsize: Figure size per image.
+        tight: Whether to use tight layout.
+        **kwargs: Keyword arguments for ``cd.imshow``.
+
+    """
+    n, images = _im_args(images)
+    if figsize is not None:
+        plt.figure(None, (figsize[0], figsize[1] * n))
+    for i, img in enumerate(images):
+        plt.subplot(1, n, i + 1)
+        imshow(img, **kwargs)
+        _tight(tight)
+        _title(titles, i)
+
+
+@register
+def imshow_col(*images, titles=None, figsize=(3, 3), tight=True, **kwargs):
+    """Imshow row.
+
+    Display a list of images in a row.
+
+    Args:
+        *images: Images.
+        titles: Titles. Either string or list of strings (one for each image).
+        figsize: Figure size per image.
+        tight: Whether to use tight layout.
+        **kwargs: Keyword arguments for ``cd.imshow``.
+
+    """
+    n, images = _im_args(images)
+    if figsize is not None:
+        plt.figure(None, (figsize[0] * n, figsize[1]))
+    for i, img in enumerate(images):
+        plt.subplot(n, 1, i + 1)
+        imshow(img, **kwargs)
+        _tight(tight)
+        _title(titles, i)
