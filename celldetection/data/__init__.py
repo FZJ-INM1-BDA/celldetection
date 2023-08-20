08fa36ac98e22b00905b79daa9fb36c1f05c4817
ericup <eric@upschulte.com>
2023-08-20 18:40:42 +0200

    Add import

--- celldetection/data/__init__.py
+++ celldetection/data/__init__.py
@@ -2,7 +2,7 @@
 This submodule contains data related code and numpy operations.
 """
 from .cpn import CPNTargetGenerator, contours2labels, render_contour, clip_contour_, masks2labels, \
-    labels2contour_list as labels2contours, contours2boxes, contours2properties
+    labels2contour_list as labels2contours, contours2boxes, contours2properties, resolve_label_channels
 from .misc import *
 from .instance_eval import LabelMatcherList, LabelMatcher
 from .segmentation import *
