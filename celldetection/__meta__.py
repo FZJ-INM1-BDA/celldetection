37f72f93a318bfa09ddc4e04dfc1a398b49cc418
ericup <eric@upschulte.com>
2023-08-31 15:38:56 +0200

    Bump version

--- celldetection/__meta__.py
+++ celldetection/__meta__.py
@@ -1,5 +1,5 @@
 __title__ = 'CellDetection'
-__version__ = '0.4.2'
+__version__ = '0.4.3'
 __author__ = 'Eric Upschulte'
 __email__ = 'e.upschulte@fz-juelich.de'
 __license__ = 'Apache License, Version 2.0'
